4cc006fb81d50a222f2d0b9972f7d6898290ca9e
andre <andrecabralpires@gmail.com>
2020-10-13 21:42:32 +0100

    changed to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_10_13_174100) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -25,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_10_13_174100) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
-    t.integer "article_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
